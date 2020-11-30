485e344e334628ce0b5cf67a38a79b4030b3ab45
Yuriy Zabegaev <zabegaev99@gmail.com>
2020-11-30 14:57:10 +0300

    fixed database bug, removed telegram token

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ COPY requirements.txt /
 
 ENV TIMEZONE=3
 ENV DATABASE_URL sqlite:///db_test
-ENV TELEGRAM_BOT_TOKEN 672100742:AAHStOe9E2ls1EmbL6-JSvHNtvVzeqPDaBU
 
 RUN pip install -r requirements.txt
 CMD [ "python", "./main.py" ]
--- src/traffic_scanner/storage/storage_sqlalchemy.py
+++ src/traffic_scanner/storage/storage_sqlalchemy.py
@@ -74,7 +74,8 @@ class TrafficStorageSQL(TrafficStorage):
     def add_route(self, start_coords, end_coords, title, user_id, s) -> Route:
         user = s.query(User).filter_by(user_id=user_id).first()
         if user is None:
-            self.update_user(User(user_id=user_id, timezone=None), s)
+            user = User(user_id=user_id, timezone=None)
+            self.update_user(user, s)
         route = Route(start_l0=start_coords[0],
                       start_l1=start_coords[1],
                       end_l0=end_coords[0],
